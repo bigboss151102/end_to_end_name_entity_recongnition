e2475990db0f2945425e278f3fdec38b5df0a4a4
hoangtrong <hoangcongtrong054@gmail.com>
2024-07-23 16:30:57 +0700

    Prepration for tranning model

---
+++ preprocess.py
@@ -0,0 +1,33 @@
+import spacy
+from spacy.tokens import DocBin
+import pickle
+
+nlp = spacy.blank("en")
+
+training_data = pickle.load(
+    open("E:/FILE of Trong/NLP Project/ner/datasets/TrainData.pickle", "rb"))
+testing_data = pickle.load(
+    open("E:/FILE of Trong/NLP Project/ner/datasets/TestData.pickle", "rb"))
+
+# the DocBin will store the example documents
+db_train = DocBin()
+for text, annotations in training_data:
+    doc = nlp(text)
+    ents = []
+    for start, end, label in annotations['entities']:
+        span = doc.char_span(start, end, label=label)
+        ents.append(span)
+    doc.ents = ents
+    db_train.add(doc)
+db_train.to_disk("E:/FILE of Trong/NLP Project/ner/datasets/train.spacy")
+
+db_test = DocBin()
+for text, annotations in training_data:
+    doc = nlp(text)
+    ents = []
+    for start, end, label in annotations['entities']:
+        span = doc.char_span(start, end, label=label)
+        ents.append(span)
+    doc.ents = ents
+    db_test.add(doc)
+db_test.to_disk("E:/FILE of Trong/NLP Project/ner/datasets/test.spacy")
