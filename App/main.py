c8f46b016d280da72d82fbc4a7ed2cf653e26b6f
hoangtrong <hoangcongtrong054@gmail.com>
2024-07-25 14:07:43 +0700

    Code class document scanner

--- App/main.py
+++ App/main.py
@@ -1,9 +1,11 @@
+from pydoc import doc
 from flask import Flask, render_template, request
 from utils import *
 
 app = Flask(__name__)
 app.secret_key = 'NER_PROJECT'
 
+document_scan = DocumentScanner()
 
 @app.route('/', methods=['GET', 'POST'])
 def scanner():
@@ -11,6 +13,10 @@ def scanner():
         fileObj = request.files['image_name']
         upload_image_path = save_upload_image(fileObj)
         print(upload_image_path)
+        #predict the coordination of the document
+        four_points, size = document_scan.document_scanner(upload_image_path)
+        print(four_points)
+        print(size)
         return render_template('scanner.html')
     return render_template('scanner.html')
 
